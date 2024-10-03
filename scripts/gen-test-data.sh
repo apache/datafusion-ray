a74ed3743499beada21dac08fda31bc6c6ff3fc5
Edmondo Porcu <edmondo.porcu@gmail.com>
2024-10-03 17:23:53 -0400

    Using TPCH Dbgen from Databricks

--- scripts/gen-test-data.sh
+++ scripts/gen-test-data.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
 
 set -e
-# Create necessary directories
-mkdir -p data
-mkdir -p test_files/tpch/data
 
-# Check if the data folder is empty
-if [ -z "$(ls -A data)" ]; then
-  echo "Data folder is empty. Cloning repository..."
-  git clone https://github.com/databricks/tpch-dbgen.git tpch/tpch-dbgen
+create_directories() {
+  mkdir -p data
+}
+
+clone_and_build_tpch_dbgen() {
+  if [ -z "$(ls -A tpch/tpch-dbgen)" ]; then
+    echo "tpch/tpch-dbgen folder is empty. Cloning repository..."
+    git clone https://github.com/databricks/tpch-dbgen.git tpch/tpch-dbgen
+    cd tpch/tpch-dbgen
+    make
+    cd ../../
+  else
+    echo "tpch/tpch-dbgen folder is not empty. Skipping cloning of TPCH dbgen."
+  fi
+}
+
+generate_data() {
   cd tpch/tpch-dbgen
-  make
-  # consistent with DataFusion test strategy
-  ./dbgen -f -s "$TPCH_SAMPLING_RATE"
-  pwd
-  ls
-  mv ./*.tbl ../../data
+  if [ "$TPCH_TEST_PARTITIONS" -gt 1 ]; then
+    for i in $(seq 1 "$TPCH_TEST_PARTITIONS"); do
+      ./dbgen -f -s "$TPCH_SCALING_FACTOR" -C "$TPCH_TEST_PARTITIONS" -S "$i"
+    done
+  else
+    ./dbgen -f -s "$TPCH_SCALING_FACTOR"
+  fi
+  mv ./*.tbl* ../../data
+}
+
+convert_data() {
   cd ../../
-  pwd
   python -m tpch.tpchgen convert --partitions "$TPCH_TEST_PARTITIONS"
-else
-  echo "Data folder is not empty. Skipping cloning and data generation."
-fi
+}
+
+main() {
+  if [ -z "$TPCH_TEST_PARTITIONS" ]; then
+    echo "Error: TPCH_TEST_PARTITIONS is not set."
+    exit 1
+  fi
+
+  if [ -z "$TPCH_SCALING_FACTOR" ]; then
+    echo "Error: TPCH_SCALING_FACTOR is not set."
+    exit 1
+  fi
+
+  create_directories
+
+  if [ -z "$(ls -A data)" ]; then
+    clone_and_build_tpch_dbgen
+    generate_data
+    convert_data
+  else
+    echo "Data folder is not empty. Skipping cloning and data generation."
+  fi
+}
+
+main
