880544aa51fe3175cd68dd8c391dd30b2c86dc5d
Vakaris Baškirov <59808854+vakarisbk@users.noreply.github.com>
2024-10-07 17:27:35 +0300

    Add documentation for testing on Kubernetes (#23)
    * add Dockerfile and docs, modify example
    
    * doc fixes

--- examples/tips.py
+++ examples/tips.py
@@ -22,11 +22,8 @@ from datafusion_ray import DatafusionRayContext
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# Start a local cluster
-ray.init(resources={"worker": 1})
-
 # Connect to a cluster
-# ray.init()
+ray.init()
 
 # Create a context and register a table
 ctx = DatafusionRayContext(2)
