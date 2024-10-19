b792eb0aaaa7704de5156bb87bc406d831be8fbf
Tim Saucer <timsaucer@gmail.com>
2024-10-19 16:09:09 -0400

    Feature/remove session context (#14)
    * Removing session context from ray context and testing against running sql query
    
    * Removing datafusion-python as a rust dependency
    
    * Add in shuffle context for decoding
    
    * Add DF ctx to init
    
    * Resolve proto compilation errors
    
    * Cargo fmt
    
    * Resolve compile warnings
    
    * Move serialization to only on rust side
    
    * Add docker ignore to reduce size of the context sent to daemon
    
    * Update Dockerfile. We shouldn't need venv inside a docker build enivornment. Also since adding the docker ignore, we can just copy in the entire directory that remains.
    
    * Remove stale code
    
    * Set up to pass in an execution plan instead of raw SQL command
    
    * Update example to show dataframe operation

--- build.rs
+++ build.rs
@@ -32,14 +32,16 @@ fn main() -> Result<(), String> {
 
     // We don't include the proto files in releases so that downstreams
     // do not need to have PROTOC included
-    if Path::new("src/proto/datafusion-ray.proto").exists() {
+    if Path::new("src/proto/datafusion_ray.proto").exists() {
+        println!("cargo:rerun-if-changed=src/proto/datafusion_common.proto");
         println!("cargo:rerun-if-changed=src/proto/datafusion.proto");
-        println!("cargo:rerun-if-changed=src/proto/datafusion-ray.proto");
+        println!("cargo:rerun-if-changed=src/proto/datafusion_ray.proto");
         tonic_build::configure()
             .extern_path(".datafusion", "::datafusion_proto::protobuf")
-            .compile(&["src/proto/datafusion-ray.proto"], &["src/proto"])
+            .extern_path(".datafusion_common", "::datafusion_proto::protobuf")
+            .compile(&["src/proto/datafusion_ray.proto"], &["src/proto"])
             .map_err(|e| format!("protobuf compilation failed: {e}"))?;
-        let generated_source_path = out.join("datafusion-ray.protobuf.rs");
+        let generated_source_path = out.join("datafusion_ray.protobuf.rs");
         let code = std::fs::read_to_string(generated_source_path).unwrap();
         let mut file = std::fs::OpenOptions::new()
             .write(true)
